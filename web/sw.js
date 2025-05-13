cf24f011c9528546592c7034c9724e7d0a2eb607
n0ll0 <101214789+n0ll0@users.noreply.github.com>
2025-05-13 21:30:44 +0300

    added sw back in

---
+++ web/sw.js
@@ -0,0 +1,36 @@
+// Basic service worker for offline support
+const CACHE_NAME = 'bdlj-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/static/styles.css',
+  '/static/Black de la Jack.svg',
+  '/static/bluetooth.svg',
+  '/static/site.webmanifest',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
